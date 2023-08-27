2216d64e72c55cefa96aedf835bd62bc608f2ab4
Charlie <cnewlinds@gmail.com>
2023-08-27 18:00:29 +1000

    re do

--- index.js
+++ index.js
@@ -27,3 +27,42 @@ document.addEventListener("keydown", function (e) {
 });
 
 
+
+
+
+
+
+/*
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+  }
+}
+
+
+
+
+const dodger = document.getElementById('dodger'); 
+  dodger.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    }
+    if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  }
+  )
+  
+/*
